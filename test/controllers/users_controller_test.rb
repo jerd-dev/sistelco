38a0dd474209891f9f9483efc2bfd198a281b868
Jonathan Ruiz <jonathanruizdublin@gmail.com>
2022-04-15 00:10:40 -0400

    prueba de test para usuarios

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+class UsersControllerTest < ActionDispatch::IntegrationTest
+    test 'render a list of users' do
+        get users_path
+
+        assert_response :success
+        assert_select '.user', 3
+    end
+
+    test 'render a detailed user page' do
+        get user_path(users(:jonathan))
+        
+        assert_response :success
+        assert_select '.nombre', 'JONATHAN RUIZ'
+        assert_select '.type_person', 'NATURAL'
+        assert_select '.telefono_p', '04241337140'
+
+    end
+end
