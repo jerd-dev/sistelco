7f598d447b8fcb1ac4a39ad16fb5773ceb61e534
Jonathan Ruiz <jonathanruizdublin@gmail.com>
2022-04-15 01:48:46 -0400

    se agrego la ruta del formulario de edicion

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get '/users/new',to: 'users#new', as: :new_user
   get '/users',to: 'users#index'
   get'/users/:id', to: 'users#show', as: :user
+  get'/users/:id/edit', to: 'users#edit', as: :edit_user
+
   
 end
