65ad4d459eb35d03b41206ebd0852ae1822ff9f9
Jonathan Ruiz <jonathanruizdublin@gmail.com>
2022-04-15 00:32:41 -0400

    se añadio la ruta de el formulario para nuevos usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # cuando se haga una peticion de tipo GET a /products se va a responder con el controlador products con el metodo index
+  get '/users/new',to: 'users#new', as: :new_user
   get '/users',to: 'users#index'
   get'/users/:id', to: 'users#show', as: :user
+  
 end
