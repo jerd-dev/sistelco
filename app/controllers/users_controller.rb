c476a89d0578b146440ec9f80da1fb5be377ecbd
Jonathan Ruiz <jonathanruizdublin@gmail.com>
2022-04-15 02:21:06 -0400

    metodo para eliminar usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path, notice: 'El usuario se ha eliminado correctamente', status: :see_other
+    end
+
 
     private
 
