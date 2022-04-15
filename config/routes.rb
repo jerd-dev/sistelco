98d9e78f07e64457434be2a145cac3a78b4957a9
Jonathan Ruiz <jonathanruizdublin@gmail.com>
2022-04-15 01:03:46 -0400

    se agrego la ruta para crear usuarios post

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # cuando se haga una peticion de tipo GET a /products se va a responder con el controlador products con el metodo index
+  post '/users',to: 'users#create'
   get '/users/new',to: 'users#new', as: :new_user
   get '/users',to: 'users#index'
   get'/users/:id', to: 'users#show', as: :user
